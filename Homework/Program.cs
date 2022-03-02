9b798820d0434ddbb2a5c7c76fad384b8d066a3e
Ekaterina <ivanova.osipenko@mail.ru>
2022-03-02 21:05:02 +0300

    Домашнее задание готово

---
+++ Homework/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+// 1 задача:
+Console.Write("Введите первое целое число: ");
+int namber1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int namber2 = Convert.ToInt32(Console.ReadLine());
+
+if(namber1 < namber2)
+{
+    Console.WriteLine("Наибольшее число: ");
+    Console.WriteLine(namber2);
+}
+if(namber1 < namber2)
+{
+    Console.WriteLine("Наименьшее число: ");
+    Console.WriteLine(namber1);
+}
+if(namber1 == namber2)
+{
+    Console.WriteLine("Оба числа равны: ");
+    Console.WriteLine(namber1 = namber2);
+}
+ 
+//2 задача:
+
+Console.Write("Введите первое целое число: ");
+int namber1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int namber2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int namber3 = Convert.ToInt32(Console.ReadLine());
+
+if(namber1 > namber2 && namber1 > namber3)
+{
+    Console.WriteLine("Максимальное число: ");
+    Console.WriteLine(namber1);
+}
+if(namber2 > namber1 && namber2 > namber3)
+{
+    Console.WriteLine("Максимальное число: ");
+    Console.WriteLine(namber2);
+}
+if(namber3 > namber1 && namber3 > namber2)
+{
+    Console.WriteLine("Максимальное число: ");
+    Console.WriteLine(namber3);
+}
+
+//3 задача:
+
+Console.Write("Введите целое число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+
+if(namber % 2 == 0)
+{
+    Console.WriteLine("Число является чётным");
+    Console.WriteLine(namber);
+}
+else
+{
+    Console.WriteLine("Число не является чётным");
+    Console.WriteLine(namber);
+}
+
+
+//4 задача:
+
+Console.Write("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+while(i <= n)
+{
+    Console.Write(i + " ");
+    i+=2;
+}
+
+*/
