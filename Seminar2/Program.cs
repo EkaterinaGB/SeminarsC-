389a41989182b933e2b2e1c7fac30f8128f709ab
Ekaterina <ivanova.osipenko@mail.ru>
2022-03-06 23:38:48 +0300

    Второй семинар оказался сложнее

---
+++ Seminar2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+
+int FindMaxNum(int num)
+{
+    int n1 = num % 10;
+    int n2 = num / 10;
+
+    if(n1 > n2)
+    {
+        return n1;
+    }
+    else
+    {
+        return n2;
+    }
+}
+
+int namber = new Random().Next(10,99);
+Console.WriteLine("Current namber is " + namber + " max part is " + FindMaxNum(namber));
+
+*/
+
